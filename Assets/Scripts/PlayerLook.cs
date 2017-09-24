5e4e5a4d6f35c2632ecd4f6a76ea8b6fd0cdc8e7
Patrick San Juan <psanjuan06.sj@gmail.com>
2017-09-23 20:54:32 -0700

    Features compelted for class 2
    - Added ability to free look
    - Move in the direction proportional to where you are looking
    - Bug: Can look up/down infinitely

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+    public float sensitivity;
+    public GameObject camera; // looking up and down
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float xspeed = Input.GetAxis("Mouse X"); //get how fast you are moving your mouse horizontally
+        transform.Rotate(Vector3.up * xspeed * sensitivity * Time.deltaTime);
+
+        float yspeed = Input.GetAxis("Mouse Y"); //looking up and down
+        print(camera.transform.eulerAngles.x);
+        camera.transform.Rotate(Vector3.left * yspeed * sensitivity * Time.deltaTime); //rotate camera
+        
+            
+
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed;
+    Vector3 moveDirection;
+    CharacterController characterController;
+    public float gravity;
+
+    void Start() {
+        characterController = GetComponent<CharacterController>(); //new
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        
+        if (!characterController.isGrounded) {
+            moveDirection.y -= gravity * Time.deltaTime; //add gravity
+        } else {
+            moveDirection.y = 0;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);  
+    }
+
+
+}
