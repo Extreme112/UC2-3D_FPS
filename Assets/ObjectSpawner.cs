b95c8b6a323e39da5a474b354d9ad0a28bb248b8
Patrick San Juan <psanjuan06.sj@gmail.com>
2017-10-07 12:01:19 -0700

    End of Class 4
    - created movement AI
    - started creating Spawners
    - Fixed Look and Movement

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+    public GameObject objToSpawn;
+    int count;
+    float delay;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnObject());
+	}
+
+    IEnumerator SpawnObject() {
+        while (count > 0) {
+            Instantiate(objToSpawn, transform.position, Quaternion.identity);
+            count--;
+            yield return new WaitForSeconds(delay);
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/AIMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //allows us to use AI
+
+public class AIMove : MonoBehaviour {
+
+    NavMeshAgent nav;       //setup of AI Movement
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        nav = GetComponent<NavMeshAgent>(); //setup of AI Movement
+        player = GameObject.FindGameObjectWithTag("Player"); //make sure to Tag 
+        //set the enemy's destination to the players position
+        nav.SetDestination(player.transform.position); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
