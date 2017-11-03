141138beb945bd2d9239d5b842daf53f6c334154
Patrick San Juan <psanjuan06.sj@gmail.com>
2017-11-03 12:02:05 -0700

    End of Class 7
    - multiple weapons, weapon switching

---
+++ Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+    public GameObject[] weapons;
+    int selectedWeapon = 0;
+	// Use this for initialization
+	void Start () {
+        SelectWeapon();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (selectedWeapon == weapons.Length - 1) {
+                selectedWeapon = 0;
+            } else {
+                selectedWeapon++;
+            }
+            SelectWeapon(); //SwitchWeapon
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            if (selectedWeapon == 0) {
+                selectedWeapon = weapons.Length - 1;
+            } else {
+                selectedWeapon--;
+            }
+            SelectWeapon();
+        }
+	}
+
+    void SelectWeapon() {
+        for (int i = 0; i < weapons.Length; i++) {
+            weapons[i].SetActive(false);
+        }
+        weapons[selectedWeapon].SetActive(true);
+    }
+}
---
+++ Assets/WeaponHolder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHolder : MonoBehaviour {
+
+    public GameObject[] weapons; //this will hold our weapons
+    int selectedWeapon = 0; //this is our default weapon
+	// Use this for initialization
+	void Start () {
+        SwitchWeapon();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SwitchWeapon() {
+        for (int i = 0; i < weapons.Length; i++) {
+            weapons[i].SetActive(false);
+        }
+        weapons[selectedWeapon].SetActive(true);
+    }
+}
