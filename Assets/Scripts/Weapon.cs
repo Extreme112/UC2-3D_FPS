5016bbe48b8860f378a4ae141e6adc645de6a24c
Patrick San Juan <psanjuan06.sj@gmail.com>
2017-11-04 15:13:26 -0700

    End of Class 8

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,29 +8,53 @@ public class Weapon : MonoBehaviour {
     public int damage;
     public GameObject oCamera;
     public float range;
-    private AudioSource audioSource; //audiosource
+    private AudioSource audioSource; 
+    //Bullet Impacts
+    public GameObject bulletImpact; //this is our bullet impact particle system
+
+    public bool isAutomatic = false; //by default, our weapon is single fire only
+    bool canFire = true; //initially, we can shoot
+    public float delay; //delay between each shot
 
     void Start() {
         audioSource = GetComponent<AudioSource>();
-        //this is the code that actually plays the audio, but it doesn't belong here
-        //where should we put this?
-        
     }
 
 	// Update is called once per frame
 	void Update () {
-        //----SHOOOTING------------
-        if (Input.GetButtonDown("Fire1")) {
-            //play audio here
-            audioSource.Play(); //this plays the shooting sound
-            RaycastHit hit;
-            if (Physics.Raycast(oCamera.transform.position, oCamera.transform.forward, out hit, range)) {
-                //  ----------Deal damage to an enemy-------
-                if (hit.collider.CompareTag("Enemy")) {
-                    EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
-                    enemyHealth.TakeDamage(damage);
-                }
+        //=======================================
+        if (isAutomatic && Input.GetButton("Fire1") && canFire) { //single shot
+            Fire();
+        }
+        if (!isAutomatic && Input.GetButtonDown("Fire1") && canFire) { //full auto
+            Fire();
+        }
+        //====================================
+    }
+
+    void Fire() {
+
+        StartCoroutine(FireLimit());
+        //play audio here
+        audioSource.Play(); //this plays the shooting sound
+        RaycastHit hit;
+        if (Physics.Raycast(oCamera.transform.position, oCamera.transform.forward, out hit, range)) {
+
+            //Create the bullet impact
+            Instantiate(bulletImpact, hit.point, Quaternion.LookRotation(hit.normal));
+
+            //  ----------Deal damage to an enemy-------
+            if (hit.collider.CompareTag("Enemy")) {
+                EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+                enemyHealth.TakeDamage(damage);
             }
         }
+        
+    }
+
+    IEnumerator FireLimit() {
+        canFire = false;
+        yield return new WaitForSeconds(delay);
+        canFire = true;
     }
 }
