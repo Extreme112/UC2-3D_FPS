353fd7bef86545cc67e82ab20df133febf816501
Patrick San Juan <psanjuan06.sj@gmail.com>
2017-11-03 23:15:46 -0700

    Ammo and Start of Ammo Pickups

---
+++ Assets/Scripts/AmmoBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Requirements for this script to work:
+// - Player game object must be tagged as "Player"
+// - Player must have a function called AddClipToSelectedWeapon()
+public class AmmoBox : MonoBehaviour {
+
+    private AudioSource audioSource;
+    bool used = false;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player") && !used) {
+            other.BroadcastMessage("AddClipToSelectedWeapon");
+            audioSource.Play();
+            StartCoroutine(DeleteThis());
+            used = true;
+        }
+    }
+
+    IEnumerator DeleteThis() {
+        while (audioSource.isPlaying) {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -16,40 +16,60 @@ public class Weapon : MonoBehaviour {
     bool canFire = true;
     public float shootDelay = 2;
 
+    //
+    public bool isAutomatic = false;
+
+    public int ammo;
+    public int maxClipSize;
+    private int currentClipSize;
+
     void Start() {
         audioSource = GetComponent<AudioSource>();
+        ammo = maxClipSize * 2;
+        //currentClipSize = maxClipSize;
     }
 
 	// Update is called once per frame
 	void Update () {
         //----SHOOOTING------------
-        if (Input.GetButtonDown("Fire1") && canFire) {
+        if (isAutomatic && Input.GetButton("Fire1") && canFire) {
+            Fire();
+        }
+        if (!isAutomatic && Input.GetButtonDown("Fire1") && canFire) {
             Fire();
         }
     }
 
     void Fire() {
-        StartCoroutine(FireLimit());
-        //play audio here
-        audioSource.Play();
-        //Muzzle Flash
-        muzzleFlash.Stop();
-        muzzleFlash.Play();
-        RaycastHit hit;
-        if (Physics.Raycast(oCamera.transform.position, oCamera.transform.forward, out hit, range)) {
-            //Show bullet impact
-            GameObject impact = Instantiate(bulletImpact, hit.point, Quaternion.LookRotation(hit.normal));
-            if (hit.collider.CompareTag("Enemy")) {
-                //Deal damage to enemy
-                EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
-                enemyHealth.TakeDamage(damage);
+        if (ammo > 0) {
+            StartCoroutine(FireLimit());
+            //play audio here
+            audioSource.Play();
+            //Muzzle Flash
+            muzzleFlash.Stop();
+            muzzleFlash.Play();
+            RaycastHit hit;
+            if (Physics.Raycast(oCamera.transform.position, oCamera.transform.forward, out hit, range)) {
+                //Show bullet impact
+                GameObject impact = Instantiate(bulletImpact, hit.point, Quaternion.LookRotation(hit.normal));
+                if (hit.collider.CompareTag("Enemy")) {
+                    //Deal damage to enemy
+                    EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+                    enemyHealth.TakeDamage(damage);
+                }
             }
+            ammo--;
         }
+        
     }
 
     IEnumerator FireLimit() {
         canFire = false;
         yield return new WaitForSeconds(shootDelay);
         canFire = true;
+    }
+
+    public void AddClip() {
+        ammo += maxClipSize;
     }
 }
